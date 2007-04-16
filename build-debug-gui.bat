d08c59aa406c84f8b2447982d3baf3a456dbfe1b
Tor <19881605+torhus@users.noreply.github.com>
2007-04-16 06:13:50 +0000

    - compile with -g for debug builds. - update CODING.TXT

--- build-debug-gui.bat
+++ build-debug-gui.bat
@@ -1,1 +1,1 @@
-build -TMonsterBrowser main.d -v1 -gui -debug -Xdwt -oddebug-gui -version=NO_STDOUT -version=OLE_COM -exec
+build -TMonsterBrowser main.d -v1 -gui -debug -g -Xdwt -oddebug-gui -version=NO_STDOUT -version=OLE_COM -exec
