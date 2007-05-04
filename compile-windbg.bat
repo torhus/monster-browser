6f4fdc7b38293ef8c996280363f817d1cb72cb4c
Tor <19881605+torhus@users.noreply.github.com>
2007-05-04 21:57:40 +0000

    - Build with the ANSI of DWT.

--- compile-windbg.bat
+++ compile-windbg.bat
@@ -3,4 +3,4 @@ call clean.bat
 @echo off
 rem set args = -L/subsystem:windows:5 
 
-dmd -o- -g -debug -version=OLE_COM -version=NO_STDOUT -L/subsystem:windows:5 main.d cvartable.d dialogs.d ini.d qstat.d settings.d lib/process.d lib/pipestream.d monitor.d parselist.d playertable.d serverlist.d servertable.d common.d launch.d dwt.lib advapi32.lib comctl32.lib gdi32.lib shell32.lib comdlg32.lib ole32.lib uuid.lib user32_dwt.lib imm32_dwt.lib shell32_dwt.lib msimg32_dwt.lib gdi32_dwt.lib kernel32_dwt.lib usp10_dwt.lib olepro32_dwt.lib oleaut32_dwt.lib oleacc_dwt.lib MonsterBrowser.exe
+dmd -o- -g -debug -version=OLE_COM -version=ANSI -version=NO_STDOUT -L/subsystem:windows:5 main.d cvartable.d dialogs.d ini.d qstat.d settings.d lib/process.d lib/pipestream.d monitor.d parselist.d playertable.d serverlist.d servertable.d common.d launch.d dwt.lib advapi32.lib comctl32.lib gdi32.lib shell32.lib comdlg32.lib ole32.lib uuid.lib user32_dwt.lib imm32_dwt.lib shell32_dwt.lib msimg32_dwt.lib gdi32_dwt.lib kernel32_dwt.lib usp10_dwt.lib olepro32_dwt.lib oleaut32_dwt.lib oleacc_dwt.lib MonsterBrowser.exe
