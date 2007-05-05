844df6f5c90658bf9406335da0be258a95a69e5f
Tor <Tor@b3265a6c-6c2a-0410-a815-fdf37b4c6c27>
2007-05-05 11:39:11 +0000

    - ANSI build doesn't work.

--- build-windbg.bat
+++ build-windbg.bat
@@ -1,1 +1,1 @@
-build -TMonsterBrowser main.d -v1 -gui -g -debug -odwindbg -Xdwt -version=OLE_COM -version=NO_STDOUT -version=ANSI
+build -TMonsterBrowser main.d -v1 -gui -g -debug -odwindbg -Xdwt -version=OLE_COM -version=NO_STDOUT
