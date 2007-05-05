90fd29d76751b5b299c7135b0e8db95661ceb3e8
Tor <19881605+torhus@users.noreply.github.com>
2007-05-05 11:39:11 +0000

    - ANSI build doesn't work.

--- build-debug-gui.bat
+++ build-debug-gui.bat
@@ -1,3 +1,3 @@
-build -TMonsterBrowser main.d -v1 -gui -debug -g -Xdwt -oddebug-gui -version=NO_STDOUT -version=OLE_COM -version=ANSI -exec
+build -TMonsterBrowser main.d -v1 -gui -debug -g -Xdwt -oddebug-gui -version=NO_STDOUT -version=OLE_COM -exec
 @rem bud -TMonsterBrowser main.d -v1 -gui -debug -g -Xdwt -oddebug-gui -version=NO_STDOUT -version=OLE_COM -exec
 @rem rebuild -ofMonsterBrowser main.d -v1 -gui -debug -g -Xdwt -oddebug-gui -version=NO_STDOUT -version=OLE_COM
--- build-profile.bat
+++ build-profile.bat
@@ -1,2 +1,2 @@
-build -TMonsterBrowser main.d -v1 -gui -debug -profile -g -Xdwt -full -clean -odprofile -version=NO_STDOUT -version=OLE_COM -version=ANSI -exec
+build -TMonsterBrowser main.d -v1 -gui -debug -profile -g -Xdwt -full -clean -odprofile -version=NO_STDOUT -version=OLE_COM -exec
 
