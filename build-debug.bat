e8227ee8a2a618638e29e0ace1158d4bc9b792c3
Tor <Tor@b3265a6c-6c2a-0410-a815-fdf37b4c6c27>
2007-06-29 04:57:31 +0000

    - Updated CODING.TXT to DMD 1.017, etc. - Added %* to the batch files' build command lines.

--- build-debug.bat
+++ build-debug.bat
@@ -1,1 +1,1 @@
-build -TMonsterBrowser main.d -v1 -oddebug -debug -g -Xdwt -version=OLE_COM -exec
+build -TMonsterBrowser main.d -v1 -oddebug -debug -g -Xdwt -version=OLE_COM -exec %*
--- build-release.bat
+++ build-release.bat
@@ -1,1 +1,1 @@
-build -TMonsterBrowser main.d -v1 -d -gui -release -O -inline -Xdwt -full -clean -version=OLE_COM -exec
+build -TMonsterBrowser main.d -v1 -d -gui -release -O -inline -Xdwt -full -clean -version=OLE_COM -exec %*
