098dadbff645e789649491167fba6038bfa6ec60
Tor <19881605+torhus@users.noreply.github.com>
2007-06-29 04:57:31 +0000

    - Updated CODING.TXT to DMD 1.017, etc. - Added %* to the batch files' build command lines.

--- build-windbg.bat
+++ build-windbg.bat
@@ -1,1 +1,1 @@
-build -TMonsterBrowser main.d -v1 -gui -g -debug -odwindbg -Xdwt -version=OLE_COM -version=NO_STDOUT
+build -TMonsterBrowser main.d -v1 -gui -g -debug -odwindbg -Xdwt -version=OLE_COM -version=NO_STDOUT %*
