96c924494df5ab77ad0a739054b71989cb3e3cff
torhus <torhus@b3265a6c-6c2a-0410-a815-fdf37b4c6c27>
2008-02-08 16:33:45 +0000

    Fixed column headers without -gui.

--- build-debug.bat
+++ build-debug.bat
@@ -1,2 +1,2 @@
 @rem build -TMonsterBrowser main.d -v1 -oddebug -debug -g -Xdwt -version=OLE_COM -exec %*
-bud -TMonsterBrowser main.d -v1 -oddebug -debug -g -Xdwt -Xphobos -version=OLE_COM -exec %*
+bud -TMonsterBrowser main.d -v1 -oddebug -debug -g -Xdwt -Xphobos -L/subsystem:console:4.0 -version=OLE_COM -exec %*
