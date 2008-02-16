b1f53b802a75f0d9dcd9fdf871f8520e1ee85016
torhus <19881605+torhus@users.noreply.github.com>
2008-02-16 22:50:50 +0000

    Use svnversion on Windows.

--- build-debug.bat
+++ build-debug.bat
@@ -1,1 +1,2 @@
-bud -TMonsterBrowser main.d -oddebug -debug -g -Xdwt -Xstd -Xtango -L/subsystem:console:4.0 -version=Tango tangobos.lib tango-user-dmd.lib debug-DD-dwt.lib %*
+@svnversion . branches/linux > svnversion.txt
+bud -TMonsterBrowser main.d -oddebug -debug -g -J. -Xdwt -Xstd -Xtango -L/subsystem:console:4.0 -version=Tango tangobos.lib tango-user-dmd.lib debug-DD-dwt.lib %*
