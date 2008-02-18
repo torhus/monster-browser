d4c2bb61fec22bc2b59b29bf5f2932bea1319369
torhus <19881605+torhus@users.noreply.github.com>
2008-02-18 07:43:59 +0000

    Fixed build-release.bat.

--- build-release.bat
+++ build-release.bat
@@ -1,1 +1,2 @@
-bud -TMonsterBrowser main.d -v1 -d -gui -release -O -inline -Xdwt -Xphobos -full -clean -version=OLE_COM -exec %*
+@svnversion . branches/linux > svnversion.txt
+bud -TMonsterBrowser main.d -gui -release -O -inline -J. -Xdwt -Xstd -Xtango -full -clean tangobos.lib tango-user-dmd.lib DD-dwt.lib %*
