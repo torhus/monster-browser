b02b89244c7de9011522eb59e06a5bcc9dd741fb
torhus <19881605+torhus@users.noreply.github.com>
2008-02-18 07:28:03 +0000

    Updated to dwt-win r161, tango r3188, dmd 1.026.  Fixed and cleaned up link.d

--- link.d
+++ link.d
@@ -11,23 +11,12 @@ version(build) {
 
 	pragma(link, "advapi32.lib");
 	pragma(link, "comctl32.lib");
+	pragma(link, "comdlg32.lib");
 	pragma(link, "gdi32.lib");
 	pragma(link, "shell32.lib");
-	pragma(link, "comdlg32.lib");
 	pragma(link, "ole32.lib");
+	pragma(link, "oleaut32.lib");
+	pragma(link, "oleacc.lib");
 	pragma(link, "msimg32.lib");
 	pragma(link, "usp10.lib");
-	pragma(link, "uuid.lib");
-	//pragma(link, "phobos.lib");
-
-	/*pragma(link, "user32_dwt.lib");
-	pragma(link, "imm32_dwt.lib");
-	pragma(link, "shell32_dwt.lib");
-	pragma(link, "msimg32_dwt.lib");
-	pragma(link, "gdi32_dwt.lib");
-	pragma(link, "kernel32_dwt.lib");
-	pragma(link, "usp10_dwt.lib");
-	pragma(link, "olepro32_dwt.lib");
-	pragma(link, "oleaut32_dwt.lib");
-	pragma(link, "oleacc_dwt.lib");*/
 }
