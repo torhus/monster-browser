58b30dc5569bbdbdaea650b42246ed0bb2c1939c
torhus <torhus@b3265a6c-6c2a-0410-a815-fdf37b4c6c27>
2008-02-25 14:27:34 +0000

    Updated the svnversion command for the switch to trunk. I also discovered svnversion's -n option.

--- build-release.bat
+++ build-release.bat
@@ -1,2 +1,2 @@
-@svnversion . branches/linux > svnversion.txt
+@svnversion -n . trunk > svnversion.txt
 bud -TMonsterBrowser main.d -version=Tango -gui -O -release -inline -J. -full -Xdwt -Xstd -Xtango -odrelease tangobos.lib tango-user-dmd.lib DD-dwt.lib %*
