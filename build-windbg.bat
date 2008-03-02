7cb8208c15c67844458cc063005d4e30033de4cc
torhus <19881605+torhus@users.noreply.github.com>
2008-03-02 21:11:36 +0000

    - Deleted obsolete batch files. - Added build-docs.bat. - Updated all the *.TXT files.

--- build-windbg.bat
+++
@@ -1,1 +0,0 @@
-bud -TMonsterBrowser main.d -v1 -gui -g -debug -odwindbg -Xdwt -Xphobos -version=OLE_COM -version=NO_STDOUT %*
--- make-package.bat
+++
@@ -1,7 +0,0 @@
-set dir=MonsterBrowser
-mkdir %dir%
-copy MonsterBrowser.exe %dir%
-copy qstat.exe %dir%
-rem copy gslist.exe %dir%
-
-set dir=
