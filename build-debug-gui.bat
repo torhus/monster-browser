ec62febb53a4b46bbf5b87b8708c3a6d0ecef945
torhus <19881605+torhus@users.noreply.github.com>
2008-04-30 21:57:10 +0000

    Made build-debug-gui.bat work again.  Fixed redirection of Cout, added redirection of Cerr.  They're now both redirected to the file DEBUG.OUT. Updated CODING.TXT to reflect these changes.  Also added mention of which versions of DMD, Tango, and DWT are currently being used to build this project.

--- build-debug-gui.bat
+++ build-debug-gui.bat
@@ -1,3 +1,2 @@
-@rem build -TMonsterBrowser main.d -v1 -gui -debug -g -Xdwt -oddebug-gui -version=NO_STDOUT -version=OLE_COM -exec %*
-bud -TMonsterBrowser main.d -v1 -gui -debug -g -Xdwt -Xphobos -oddebug-gui -version=NO_STDOUT -version=OLE_COM -exec %*
-@rem rebuild -ofMonsterBrowser main.d -v1 -gui -debug -g -Xdwt -oddebug-gui -version=NO_STDOUT -version=OLE_COM
+@svnversion -n . trunk > svnversion.txt
+bud -TMonsterBrowser main.d -oddebug-gui -debug -g -gui -J. -Xdwt -Xtango -version=Tango -version=redirect tango-user-dmd.lib debug-DD-dwt.lib %*
