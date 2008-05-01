f98d57b48afb497be5451be8a17feb2d861f8513
torhus <torhus@b3265a6c-6c2a-0410-a815-fdf37b4c6c27>
2008-05-01 16:39:49 +0000

    Updated to Tango 0.99.6 and DWT rev. 211:ff59aeb96cac.

--- build-debug.bat
+++ build-debug.bat
@@ -1,2 +1,2 @@
 @svnversion -n . trunk > svnversion.txt
-bud -TMonsterBrowser main.d -oddebug -debug -g -J. -Xdwt -Xtango -L/subsystem:console:4.0 -version=Tango tango-user-dmd.lib debug-DD-dwt.lib %*
+bud -TMonsterBrowser main.d -oddebug -debug -g -J. -Xdwt -Xtango -L/subsystem:console:4.0 -L/rc:dwt.res -version=Tango tango-user-dmd.lib debug-DD-dwt.lib %*
