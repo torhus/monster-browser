466752ce9182a2bf5395abf561b37d29d584b8da
torhus <torhus@b3265a6c-6c2a-0410-a815-fdf37b4c6c27>
2008-05-16 04:52:21 +0000

    Added display of flags.  Dropped the global Display instance.

--- servertable.d
+++ servertable.d
@@ -1,10 +1,15 @@
 module servertable;
 
+import tango.text.Util;
+
 import dwt.DWT;
 import dwt.events.SelectionAdapter;
 import dwt.events.SelectionEvent;
+import dwt.graphics.Image;
+import dwt.graphics.Rectangle;
 import dwt.graphics.TextLayout;
 import dwt.widgets.Composite;
+import dwt.widgets.Display;
 import dwt.widgets.Event;
 import dwt.widgets.Listener;
 import dwt.widgets.Table;
@@ -13,6 +18,7 @@ import dwt.widgets.TableItem;
 
 import colorednames;
 import common;
+import geoip;
 import launch;
 import main;
 import serverlist;
@@ -67,6 +73,8 @@ class ServerTable
 			}
 		});
 
+		drawFlags_ = initGeoIp();
+
 		/* Allow users to disable colored server names, since drawing them is so
 		 * slow.
 		 */
@@ -86,18 +94,33 @@ class ServerTable
 					TableItem item = cast(TableItem) e.item;
 					auto i = table_.indexOf(item);
 					ServerData* sd = getActiveServerList.getFiltered(i);
-					TextLayout tl = sd.customData;
 
+					enum { leftMargin = 2, flagWidth = 18 }
+
+					// draw flag
+					if (drawFlags_) {
+						char[] ip = sd.server[ServerColumn.ADDRESS];
+						auto colon = locate(ip, ':');
+						char[] country = countryCodeByAddr(ip[0..colon]);
+						// FIXME: cache the flag?
+						Image flag = getFlagImage(country);
+						if (flag)
+							e.gc.drawImage(flag, e.x+leftMargin, e.y);
+					}
+
+					// draw text
+					TextLayout tl = sd.customData;
 					if (tl is null) {
 						auto parsed = parseColors(sd.rawName);
-						tl = new TextLayout(display);
+						tl = new TextLayout(Display.getDefault);
 						tl.setText(parsed.cleanName);
 						foreach (r; parsed.ranges)
 							tl.setStyle(r.style, r.start, r.end);
 
 						sd.customData = tl;  // cache it
 					}
-					tl.draw(e.gc, e.x+2, e.y);
+					auto textX = e.x + leftMargin + flagWidth + 4;
+					tl.draw(e.gc, textX, e.y);
 				}
 			});
 		}
@@ -280,6 +303,7 @@ private:
 	Table table_;
 	Composite parent_;
 	char[] selectedIp_ = "";
+	bool drawFlags_ = false;
 
 	private int getBottomIndex()
 	{
