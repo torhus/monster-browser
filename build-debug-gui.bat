9a01eceec0dbe5c5f48d4a47da4421a37419de53
torhus <19881605+torhus@users.noreply.github.com>
2008-05-26 21:42:31 +0000

    Embed flags in the executable, since loading them from a zip file was too slow. This means that zlib is no longer a dependency.

--- build-debug-gui.bat
+++ build-debug-gui.bat
@@ -1,2 +1,2 @@
 @svnversion -n . trunk > svnversion.txt
-bud -TMonsterBrowser main.d -oddebug-gui -debug -g -gui -J. -Xdwt -Xtango -version=Tango -version=redirect -L/rc:dwt.res tango-user-dmd.lib debug-DD-dwt.lib zlib.lib %*
+bud -TMonsterBrowser main.d -oddebug-gui -debug -g -gui -J. -Xdwt -Xtango -version=Tango -version=redirect -L/rc:dwt.res tango-user-dmd.lib debug-DD-dwt.lib %*
--- build-profile.bat
+++ build-profile.bat
@@ -1,2 +1,2 @@
 @svnversion -n . trunk > svnversion.txt
-bud -TMonsterBrowser main.d -version=Tango -profile -O -release -g -J. -Xdwt -Xtango -odprofile -L/rc:dwt.res tango-user-dmd.lib DD-dwt.lib zlib.lib %*
+bud -TMonsterBrowser main.d -version=Tango -profile -O -release -g -J. -Xdwt -Xtango -odprofile -L/rc:dwt.res tango-user-dmd.lib DD-dwt.lib %*
