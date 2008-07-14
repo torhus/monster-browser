d7c24261936e511668f1f295f8f0cb587e1fd8c0
torhus <19881605+torhus@users.noreply.github.com>
2008-07-14 23:36:51 +0000

    Application icon.  Just code, no icon yet.

--- build-debug-gui.bat
+++ build-debug-gui.bat
@@ -1,2 +1,2 @@
 @svnversion -n . trunk > svnversion.txt
-bud -TMonsterBrowser main.d -oddebug-gui -debug -g -gui -J. -Xdwt -Xtango -version=Tango -version=redirect -L/rc:dwt.res tango-user-dmd.lib debug-DD-dwt.lib %*
+bud -TMonsterBrowser main.d -oddebug-gui -debug -g -gui -J. -Xdwt -Xtango -version=Tango -version=redirect -L/rc:dwt.res -L/rc:mb.res tango-user-dmd.lib debug-DD-dwt.lib %*
