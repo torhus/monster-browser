246b3874a661985a90914dadc9f124f7a21c1ee5
torhus <torhus@b3265a6c-6c2a-0410-a815-fdf37b4c6c27>
2008-07-16 22:31:25 +0000

    Updated: DMD 1.027 -> 1.033 DWT r211 -> r254 Tango 0.99.6 -> r3753

--- build-profile.bat
+++ build-profile.bat
@@ -1,2 +1,2 @@
 @svnversion -n . trunk > svnversion.txt
-bud -TMonsterBrowser main.d -version=Tango -profile -O -release -g -J. -Xdwt -Xtango -odprofile -L/rc:dwt.res -L/rc:mb.res tango-user-dmd.lib DD-dwt.lib %*
+bud -TMonsterBrowser main.d -version=Tango -version=TANGOSVN -profile -O -release -g -J. -Xdwt -Xtango -odprofile -L/rc:dwt.res -L/rc:mb.res tango-user-dmd.lib DD-dwt.lib %*
