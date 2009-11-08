f72a274af60e00fb4daa7d76136c42cf27b21c6c
torhus <torhus@b3265a6c-6c2a-0410-a815-fdf37b4c6c27>
2009-11-08 22:28:45 +0000

    - Switched to xfBuild for simpler building on Windows. - Updated to DMD 1.051, Tango trunk r5035, and dwt-win r341.

--- build-debug-gui.bat
+++ build-debug-gui.bat
@@ -1,2 +1,2 @@
 @svnversion -n . trunk > svnversion.txt 
-bud -TMonsterBrowser main.d -oddebug-gui -debug -g -gui -J. -Jflags -Jicons -Jres -Xdwt -Xtango -version=Tango -version=TANGOSVN -version=redirect -L/rc:dwt.res -L/rc:mb.res tango-user-dmd.lib debug-DD-dwt.lib %*
+xfbuild main.d +oMonsterBrowser +Odebug-gui +Ddebug-gui/.deps -debug -g -J. -Jflags -Jicons -Jres +xtango -L/subsystem:windows:4 -L/rc:dwt.res -L/rc:mb.res -version=TANGOSVN tango-user-dmd.lib %*
