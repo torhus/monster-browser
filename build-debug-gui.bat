27c00abb03457a07a2a1cd6490ddd5c362656def
torhus <19881605+torhus@users.noreply.github.com>
2011-12-22 13:26:42 +0000

    -version=TANGOSVN no longer needed.

--- build-debug-gui.bat
+++ build-debug-gui.bat
@@ -1,2 +1,2 @@
 @svnversion -n . trunk > svnversion.txt 
-xfbuild main.d +oMonsterBrowser +Odebug-gui +Ddebug-gui/.deps -debug -g -J. -Jflags -Jicons -Jres +xtango -L/subsystem:windows:4 -L/rc:dwt.res -L/rc:mb.res -version=TANGOSVN %*
+xfbuild main.d +oMonsterBrowser +Odebug-gui +Ddebug-gui/.deps -debug -g -J. -Jflags -Jicons -Jres +xtango -L/subsystem:windows:4 -L/rc:dwt.res -L/rc:mb.res %*
