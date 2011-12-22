7f1c4d2d9b158f1db7d4985a5478a310008660b4
torhus <torhus@b3265a6c-6c2a-0410-a815-fdf37b4c6c27>
2011-12-22 13:26:42 +0000

    -version=TANGOSVN no longer needed.

--- build-docs.bat
+++ build-docs.bat
@@ -1,6 +1,6 @@
 @echo off
-rem bud main.d -Dddocs -J. -Jflags -Jicons -Jres -full -obj -o- -clean -version=TANGOSVN -Xdwt -Xtango -Xphobos -Xlib -Xini -Xlink %*
-rem xfbuild main.d -Dddocs +Oddocs +Ddocs/.deps -o- -J. -Jflags -Jicons -Jres +xtango +xdwt +xini -version=TANGOSVN %*
+rem bud main.d -Dddocs -J. -Jflags -Jicons -Jres -full -obj -o- -clean -Xdwt -Xtango -Xphobos -Xlib -Xini -Xlink %*
+rem xfbuild main.d -Dddocs +Oddocs +Ddocs/.deps -o- -J. -Jflags -Jicons -Jres +xtango +xdwt +xini %*
 
 setlocal
 set files=colorednames.d common.d cvartable.d dialogs.d flagdata.d genflagdata.d geoip.d launch.d link.d main.d mainwindow.d masterlist.d messageboxes.d playertable.d qstat.d rcon.d runtools.d serveractions.d serverdata.d serverlist.d serverqueue.d servertable.d set.d settings.d threadmanager.d mswindows/taskbarlist.d mswindows/taskbarprogress.d mswindows/util.d
