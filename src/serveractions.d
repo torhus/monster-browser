torhus/monster-browser

commit c1a992989a47b572326263492a1f91071349cc84

Add gameTypes option in mods.ini
See #10.

src/serveractions.d
@@ -141,7 +141,7 @@ void switchToGame(in char[] name)
 
 	}
 
-	gameName = name;
+	gameName = (findString(gameNames, name) != -1) ? name : gameNames[0];
 	threadManager.run({ Display.getDefault().syncExec(dgRunnable(&f)); });
 }
 

src/dialogs.d
@@ -439,6 +439,7 @@ class SettingsDialog
 						if (modified(gamesFileName) > lastModified_) {
 							settings.loadGamesFile();
 							filterBar.setGames(settings.gameNames);
+							switchToGame(serverTable.serverList.gameName);
 						}
 					}
 					catch (IOException e) {

src/masterlist.d
@@ -449,8 +449,15 @@ private final class MySaxHandler(Ch=char) : SaxHandler!(Ch)
 			else if (attr.localName == "value")
 				cvar[1] = fromEntityCopy(attr.value);
 
-			if (icompare(cvar[0], "g_gametype") == 0)
-				servers[$-1].server[ServerColumn.GAMETYPE] = cvar[1];
+			if (icompare(cvar[0], "g_gametype") == 0) {
+				uint ate;
+				int gt = Integer.parse(cvar[1], 10, &ate);
+
+				if (ate == cvar[1].length)
+					servers[$-1].numericGameType = gt;
+				else
+					servers[$-1].numericGameType = -1;
+			}
 			else if (icompare(cvar[0], "g_needpass") == 0) {
 				char[] s = cvar[1] == "0" ? PASSWORD_NO : PASSWORD_YES;
 				servers[$-1].server[ServerColumn.PASSWORDED] = s;

src/qstat.d
@@ -43,7 +43,6 @@ private enum Field {
 bool parseOutput(in char[] modName, Lines!(char) iter,
                 bool delegate(ServerData*, bool replied) deliver)
 {
-	char[][] gtypes;
 	scope BufferedOutput outfile = null;
 	debug StopWatch timer2;
 	debug timer2.start();
@@ -66,13 +65,6 @@ bool parseOutput(in char[] modName, Lines!(char) iter,
 	scope (exit) {
 		if (outfile)
 			outfile.flush.close;
-	}
-
-	if (modName in gameTypes) {
-		gtypes = gameTypes[modName];
-	}
-	else {
-		gtypes = defaultGameTypes;
 	}
 
 	while (keepGoing) {
@@ -111,7 +103,7 @@ bool parseOutput(in char[] modName, Lines!(char) iter,
 					outfile.write(newline);
 				}
 
-				parseCvars(line, &sd, gtypes);
+				parseCvars(line, &sd);
 
 				sortStringArray(sd.cvars);
 
@@ -167,23 +159,18 @@ debug private void checkTime(ref StopWatch t, char[] name)
 /**
  * Parses a line of cvars, each cvar of the form "name=value".  If there's more
  * than one cvar, FIELDSEP is expected to separate each name/value pair.  The
- * cvars are appended to sd.cvars.  The gametype and password fields of
- * sd.server are also set.
+ * cvars are appended to sd.cvars.
  *
  * The strings that are the output of this function will be slices into an
  * heap-allocated copy of the line parameter.
  *
  * Params:
  *     line     = String to parse.
- *     sd       = Output, only sd.cvars, sd.server, and sd.protocolVersion are
- *                changed.  sd.rawName is used for error reporting, but not
- *                written to.
- *     gtypes   = Game type names, indexed with the value of the 'gametype'
- *                cvar to find the value of sd.server's Game type column.  If
- *                gametype >= gtypes.length, the number is used instead.
+ *     sd       = Output, only sd.cvars, sd.server, sd.numericGameType,
+ *                and sd.protocolVersion are changed.  sd.rawName is used for
+ *                error reporting, but not written to.
  */
-private void parseCvars(in char[] line, ServerData* sd,
-                                                       in char[][] gtypes=null)
+private void parseCvars(in char[] line, ServerData* sd)
 in {
 	assert(ServerColumn.GAMETYPE < sd.server.length &&
 	                               ServerColumn.PASSWORDED < sd.server.length);
@@ -199,15 +186,12 @@ body {
 			case "gametype":
 				uint ate;
 				int gt = parse(cvar[1], 10, &ate);
-				if (ate < cvar[1].length) {
-					invalidInteger(sd.rawName, cvar[1]);
-					sd.server[ServerColumn.GAMETYPE] = "???";
-				}
-				else if (gt < gtypes.length) {
-					sd.server[ServerColumn.GAMETYPE] = gtypes[gt];
+				if (ate == cvar[1].length) {
+					sd.numericGameType = gt;
 				}
 				else {
-					sd.server[ServerColumn.GAMETYPE] = toString(gt);
+					invalidInteger(sd.rawName, cvar[1]);
+					sd.numericGameType = -1;
 				}
 				break;
 			case "g_needpass":

src/serverdata.d
@@ -16,6 +16,8 @@ import settings;
 struct ServerData {
 	/// server name, with any color codes intact
 	char[] rawName;
+	/// g_gametype cvar's value, or -1 if missing or invalid.
+	int numericGameType = -1;
 	/// name (without color codes), ping, playercount, map, etc.
 	/// Note: If this is a zero-length array, this object is considered to be
 	/// empty, and can be deleted.
@@ -127,6 +129,7 @@ const char[] TIMEOUT = "9999";
 void setEmpty(ServerData* sd)
 {
 	sd.rawName = null;
+	sd.numericGameType = -1;
 	sd.server  = null;
 	sd.players = null;
 	sd.cvars   = null;
@@ -187,6 +190,31 @@ bool matchGame(in ServerData* sd, in GameConfig game)
 		return true;
 }
 
+
+/**
+ * Returns the game type name corresponding to the given the numeric type.
+ */
+char[] getGameTypeName(in GameConfig game, int type)
+{
+	char[][] gtypes = game.gameTypes;
+
+	if (type < 0) {
+		return "???";
+	}
+
+	if (gtypes is null) {
+		// Fall back to hardcoded names or Q3 defaults.
+		char[][]* t = game.mod in gameTypes;
+		gtypes = t ? *t : defaultGameTypes;
+	}
+
+	if (type < gtypes.length) {
+		return gtypes[type];
+	}
+	else {
+		return Integer.toString(type);
+	}
+}
 
 /// Did this server time out when last queried?
 bool timedOut(in ServerData* sd)

src/serverlist.d
@@ -106,6 +106,8 @@ final class ServerList
 
 			if (address in ipHash_ && matchGame(&sd, game)) {
 				newHash[address] = -1;
+				sd.server[ServerColumn.GAMETYPE] =
+				                     getGameTypeName(game, sd.numericGameType);
 				if (!isFilteredOut(&sd))
 					filteredList ~= sh;
 			}
@@ -337,11 +339,15 @@ private:
 	{
 		synchronized (this) synchronized (master_) {
 			ServerData sd = master_.getServerData(sh);
+			GameConfig game = getGameConfig(gameName_);
 			bool removed = false;
 
 			if (replace) {
 				removed = removeFromFiltered(sd.server[ServerColumn.ADDRESS]);
 			}
+
+			sd.server[ServerColumn.GAMETYPE] =
+			                         getGameTypeName(game, sd.numericGameType);
 
 			if (!removed) {
 				// adding as a new server

src/settings.d
@@ -50,6 +50,18 @@ struct GameConfig
 		return section.getValue("protocolVersion", "68");
 	}
 
+	char[][] gameTypes() ///
+	{
+		char[] s = section.getValue("gameTypes");
+
+		if (s is null)
+			return null;
+
+		char[][] r = split(trim(s), " ");
+
+		return r[0].length > 0 ? r : r[0..0];
+	}
+
 	char[] extraServersFile() /// Like "baseq3.extra".
 	{
 		return appDir ~ mod ~ ".extra";  // FIXME: check dataDir too?
@@ -142,6 +154,7 @@ private {
 ;           executable on explicitly, like this: C:\TheGame\game.exe %*
 ; masterServer    - defaults to master.quake3arena.com:27950.
 ; protocolVersion - defaults to 68
+; gameTypes       - List of game type names, seperated by spaces.
 ; etColors - Set to true to enable Enemy Territory-style extended color codes (31 colors).
 ;            See http://wolfwiki.anime.net/index.php/Color_Codes for more information.
 ; qstatMasterServerType - Defaults to quake3arenam.

commit 0e9f359379cb123197e69ab35965dd259ca14df9

Fix bug introduced in c1a992989a47b572326263492a1f91071349cc84

src/serveractions.d
@@ -141,7 +141,7 @@ void switchToGame(in char[] name)
 
 	}
 
-	gameName = (findString(gameNames, name) != -1) ? name : gameNames[0];
+	gameName = name;
 	threadManager.run({ Display.getDefault().syncExec(dgRunnable(&f)); });
 }
 