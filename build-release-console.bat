553a1b5fbccb7b483a9b89f4578f39c7071a3220
torhus <torhu@yahoo.com>
2022-09-10 15:56:17 +0200

    Always do full builds to avoid linking errors

--- build-release-console.bat
+++ build-release-console.bat
@@ -1,2 +1,2 @@
 @svnversion -n . trunk > svnversion.txt
-xfbuild main.d +oMonsterBrowser +Orelease-console +Drelease-console/.deps -O -release -inline -J. -Jflags -Jicons -Jres +xstd +xcore -L/subsystem:console:4 -L/rc:dwt.res -L/rc:mb.res %*
+xfbuild main.d +oMonsterBrowser +Orelease-console +Drelease-console/.deps -O -release -inline -J. -Jflags -Jicons -Jres +xstd +xcore -L/subsystem:console:4 -L/rc:dwt.res -L/rc:mb.res +full %*
--- build-release.bat
+++ build-release.bat
@@ -1,2 +1,2 @@
 @svnversion -n . trunk > svnversion.txt
-xfbuild main.d +oMonsterBrowser +Orelease +Drelease/.deps -O -release -inline -J. -Jflags -Jicons -Jres +xstd +xcore -L/subsystem:windows:4 -L/rc:dwt.res -L/rc:mb.res %*
+xfbuild main.d +oMonsterBrowser +Orelease +Drelease/.deps -O -release -inline -J. -Jflags -Jicons -Jres +xstd +xcore -L/subsystem:windows:4 -L/rc:dwt.res -L/rc:mb.res +full %*
