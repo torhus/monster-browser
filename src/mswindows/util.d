fecbb02ae35c81949a629b0ecccc60619058648f
torhus <torhu@yahoo.com>
2022-11-22 20:23:09 +0100

    Switch to the UTF-16 Windows API functions

--- src/mswindows/util.d
+++ src/mswindows/util.d
@@ -8,7 +8,6 @@ public import core.sys.windows.shlobj;
 import core.sys.windows.windows;
 import std.conv;
 import std.string;
-import std.windows.charset;
 import std.windows.syserror;
 
 
@@ -47,10 +46,8 @@ string getProgramFilesDirectory()
 
 /**
  * Returns the value of a registry key, or null if there was an error.
-
- * Throws: Exception if the argument is not a valid key.
  *
- * BUGS: Doesn't convert arguments to ANSI.
+ * Throws: Exception if the argument is not a valid key.
  */
 string getRegistryStringValue(in char[] key)
 {
@@ -60,44 +57,38 @@ string getRegistryStringValue(in char[] key)
     LPBYTE lpData = buf.ptr;
     DWORD dwSize = buf.length;
     LONG status;
-    const(char)[] retval = null;
+    string retval = null;
 
-    const(char)[][] parts = split(key, "\\");
+    wstring[] parts = split(to!wstring(key), "\\");
     if (parts.length < 3)
         throw new Exception("Invalid registry key: " ~ cast(string)key);
 
     HKEY keyConst = hkeyFromString(parts[0]);
-    // cast here because join takes immutable instead of const
-    const(char)[] subKey = join(cast(string[])parts[1..$-1], "\\");
-    const(char)[] name = parts[$-1];
-
-    status = RegOpenKeyExA(keyConst, cast(char*)toStringz(subKey), 0,
-                                                       KEY_ALL_ACCESS, &hKey);
+    wchar* subKey = join(parts[1..$-1], "\\").dup.ptr;
+    status = RegOpenKeyExW(keyConst, subKey, 0, KEY_ALL_ACCESS, &hKey);
 
     if (status == ERROR_SUCCESS) {
-        const(char)* namez = toStringz(name);
-        status = RegQueryValueExA(hKey, namez, null, &dwType, lpData, &dwSize);
+        wchar* name = parts[$-1].dup.ptr;
+        status = RegQueryValueExW(hKey, name, null, &dwType, lpData, &dwSize);
 
         if (status == ERROR_MORE_DATA) {
             lpData = (new BYTE[dwSize]).ptr;
-            status = RegQueryValueExA(hKey, namez, null,
+            status = RegQueryValueExW(hKey, name, null,
                                                      &dwType, lpData, &dwSize);
         }
 
         if (status == ERROR_SUCCESS)
-            retval = fromMBSz(cast(immutable char*)lpData);
+            retval = to!string(cast(wchar*)lpData);
 
         RegCloseKey(hKey);
     }
 
-    // FIXME: need to cast to void* because of a DMD bug
-    return (cast(void*)retval.ptr == cast(void*)buf.ptr) ? retval.idup :
-                                                            cast(string)retval;
+    return retval;
 }
 
 
 /// Throws: Exception.
-private HKEY hkeyFromString(in char[] s)
+private HKEY hkeyFromString(in wchar[] s)
 {
     if (icmp(s, "HKEY_CLASSES_ROOT") == 0)
         return HKEY_CLASSES_ROOT;
@@ -106,5 +97,5 @@ private HKEY hkeyFromString(in char[] s)
     if (icmp(s, "HKEY_LOCAL_MACHINE") == 0)
         return HKEY_LOCAL_MACHINE;
 
-    throw new Exception("Invalid HKEY: " ~ cast(string)s);
+    throw new Exception("Invalid HKEY: " ~ to!string(s));
 }
