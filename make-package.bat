be6f3a6348c40249354cf1503e843ddd6b52896d
torhus <torhu@yahoo.com>
2022-12-09 22:12:53 +0100

    Convert text files to CRLF when creating Windows packages

--- make-package.bat
+++ make-package.bat
@@ -11,6 +11,7 @@ qstat_mb.cfg ^
 README.TXT
 
 md package\MonsterBrowser\
+unix2dos -k CHANGELOG.TXT mods-default.ini portable.txt qstat_mb.cfg README.TXT
 for %%f in (%FILES%) do copy %%f package\MonsterBrowser\
 cd package
 7z a ..\MonsterBrowser09i.zip MonsterBrowser
