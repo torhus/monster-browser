b58352c88aa30f937f1dc929a854c1fdcbebf3a0
torhus <torhu@yahoo.com>
2026-04-02 15:51:42 +0200

    Release v1.0a

--- make-package.bat
+++ make-package.bat
@@ -3,7 +3,6 @@ CHANGELOG.TXT ^
 GeoLite2-Country.mmdb ^
 libcurl.dll ^
 libmaxminddb.dll ^
-games-default.ini ^
 MonsterBrowser.exe ^
 portable.txt ^
 qstat.exe ^
@@ -11,9 +10,9 @@ qstat_mb.cfg ^
 README.TXT
 
 md package\MonsterBrowser\
-unix2dos -k CHANGELOG.TXT games-default.ini portable.txt qstat_mb.cfg README.TXT
+unix2dos -k CHANGELOG.TXT portable.txt qstat_mb.cfg README.TXT
 for %%f in (%FILES%) do copy %%f package\MonsterBrowser\
 cd package
-7z a ..\MonsterBrowser10.zip MonsterBrowser
+7z a ..\MonsterBrowser10a.zip MonsterBrowser
 cd ..
 rd /S /Q package
